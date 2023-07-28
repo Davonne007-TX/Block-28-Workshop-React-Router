b44814ec501d8fcc5708f6e4dd8be79c5d8423f4
Vonney <davonnejv@gmail.com>
2023-07-28 04:18:12 -0500

    adding react router and Routes and Links

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -4,9 +4,27 @@ function App() {
 
   return (
     <>
-      <div>
+      <div id="container">
+        <h1>Aloha React Router...!</h1>
 
-        hi
+        <div id="navBar">
+          <nav>
+            <ul>
+              <li><Link to="/blue" element={<Blue />}>Blue</Link></li>
+            </ul>
+          </nav>
+        </div>
+
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/pink" element={<Pink />} />
+          </Routes>
+        </div>
+
+
+     
       </div>
     </>
   )
