c1b1e28e1cf728192e1b499293dd8ef412c2d863
Vonney <davonnejv@gmail.com>
2023-07-28 06:15:30 -0500

    adding styles to NavBar

--- react-router/src/components/NavBar.jsx
+++ react-router/src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './NavBar.css'
 
 function NavBar() {
   return (
     <nav>
-        <ul>
+        <ul className="myLinks">
             <li><Link to="/home">Home</Link></li>
             <li><Link to="/blue">Blue</Link></li>
             <li><Link to="/red">Red</Link></li>
