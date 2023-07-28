55dcb2e51cec8c865c10d6a9832d74c68264ad78
Vonney <davonnejv@gmail.com>
2023-07-28 04:48:12 -0500

    adding Link and Route for Home Component, and imported it

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Blue from './components/Blue'
 import Red from './components/Red'
 import Pink from './components/Pink'
+import Home from './components/Home'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <div id="navbar">
           <nav>
             <ul>
+              <li><Link to="/home">Home</Link></li>
               <li><Link to="/blue">Blue</Link></li>
               <li><Link to="/red">Red</Link></li>
               <li><Link to="/pink">Pink</Link></li>
@@ -21,6 +23,7 @@ function App() {
 
         <div id="main-section">
           <Routes>
+            <Route path="/home" element={<Home />} />
             <Route path="/blue" element={<Blue />} />
             <Route path="/red" element={<Red />} />
             <Route path="/pink" element={<Pink /> } />
