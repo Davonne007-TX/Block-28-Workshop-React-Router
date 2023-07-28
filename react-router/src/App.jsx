d69d877975623cad3287ce315c041746321b70e5
Vonney <davonnejv@gmail.com>
2023-07-28 04:29:49 -0500

    adding links within navbar

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,31 +1,29 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <div id="container">
-        <h1>Aloha React Router...!</h1>
-
-        <div id="navBar">
+        <div id="navbar">
           <nav>
             <ul>
-              <li><Link to="/blue" element={<Blue />}>Blue</Link></li>
+              <li><Link to="/blue">Blue</Link></li>
+              <li><Link to="/red">Red</Link></li>
+              <li><Link to="/pink">Pink</Link></li>
             </ul>
           </nav>
         </div>
 
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} />
-            <Route path="/pink" element={<Pink />} />
+            <Route path="/blue" element={<h1>Blue</h1>} />
+            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/pink" element={<h1>Pink</h1>} />
           </Routes>
         </div>
-
-
-     
-      </div>
+      </div> {/*last div to container */}
     </>
   )
 }
