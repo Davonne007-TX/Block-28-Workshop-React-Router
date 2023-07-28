84bee4de94389f1cb5b6260032395f7b5d9fbe44
Vonney <davonnejv@gmail.com>
2023-07-28 05:30:45 -0500

    redo

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,36 +1,14 @@
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
-import Blue from './components/Blue'
-import Red from './components/Red'
-import Pink from './components/Pink'
-import Home from './components/Home'
+
 
 function App() {
 
   return (
     <>
-      <div id="container">
-        <div id="navbar">
-          <nav>
-            <ul>
-              <li><Link to="/home">Home</Link></li>
-              <li><Link to="/blue">Blue</Link></li>
-              <li><Link to="/red">Red</Link></li>
-              <li><Link to="/pink">Pink</Link></li>
-            </ul>
-          </nav>
-        </div>
 
-        <div id="main-section">
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} />
-            <Route path="/pink" element={<Pink /> } />
-          </Routes>
-        </div>
-      </div> {/*last div to container */}
+    hi
     </>
+    
   )
 }
 
