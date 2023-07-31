9e5e0e6dab57b442c3fdb397a6566f9b8fbeaf22
Vonney <davonnejv@gmail.com>
2023-07-31 02:41:05 -0500

    adding black power ranger to home

--- react-router/src/components/Home.jsx
+++ react-router/src/components/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
       <div className="home">
         <h1>Home</h1>
         <img src="../public/images/logo.png" className="logo"/>
+        <img src="../public/images/blackRanger.png" className="powerRangerImage"/>
      </div>
    </>
   )
