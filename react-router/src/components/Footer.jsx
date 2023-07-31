f05b560b8ce9a225e0204c3578e6785d7aa96037
Vonney <davonnejv@gmail.com>
2023-07-31 02:01:18 -0500

    adding styles to footer

--- react-router/src/components/Footer.jsx
+++ react-router/src/components/Footer.jsx
@@ -8,12 +8,12 @@ function Footer() {
     <footer>
        <h3>Back to Pages</h3>
          <div className="footerLinks">
-           <li><Link to="/home">Home</Link></li>
-           <li><Link to="/blue">Blue</Link></li>
-           <li><Link to="/red">Red</Link></li>
-           <li><Link to="/pink">Pink</Link></li>
-           <li><Link to="/green">Green</Link></li>
-           <li><Link to="/yellow">Yellow</Link></li>
+           <li><Link to="/home" className="homeLink">Home</Link></li>
+           <li><Link to="/blue" className="blueLink">Blue</Link></li>
+           <li><Link to="/red" className="redLink">Red</Link></li>
+           <li><Link to="/pink" className="pinkLink">Pink</Link></li>
+           <li><Link to="/green" className="greenLink">Green</Link></li>
+           <li><Link to="/yellow" className="yellowLink">Yellow</Link></li>
          </div>
     </footer>
     </>
