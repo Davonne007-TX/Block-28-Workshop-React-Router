8bb9525b1b6320ceeeb887a7577d28817074fff3
Vonney <davonnejv@gmail.com>
2023-08-02 21:04:38 -0500

    adding useNavigate to Footer.jsx

--- react-router/src/components/Footer.jsx
+++ react-router/src/components/Footer.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import './Footer.css'
-import { Link } from 'react-router-dom'
+import { useNavigate} from 'react-router-dom'
 
 function Footer() {
+
+  const navigate = useNavigate()
+
   return (
     <>
     <footer>
        <h3>Back to Pages</h3>
          <div className="footerLinks">
-           <li><Link to="/home" className="homeLink">Home</Link></li>
-           <li><Link to="/blue" className="blueLink">Blue</Link></li>
-           <li><Link to="/red" className="redLink">Red</Link></li>
-           <li><Link to="/pink" className="pinkLink">Pink</Link></li>
-           <li><Link to="/green" className="greenLink">Green</Link></li>
-           <li><Link to="/yellow" className="yellowLink">Yellow</Link></li>
-         </div>
+           <button onClick={() => navigate('./home')}  className="homeLink">Home</button>
+           <button onClick={() => navigate('./blue')} className="blueLink">Blue</button>
+           <button onClick={()=> navigate('./red')}  className="redLink">Red</button>
+           <button onClick={() => navigate('./pink')} className="pinkLink">Pink</button>
+           <button onClick={() => navigate('./green')} class="greenLinksName=">Green</button>
+           <button onClick={() => navigate('./yellow')} className="yellowLink">Yellow</button>
+         </div> 
     </footer>
     </>
   )
 }
 
 export default Footer
+
+
+
 
 
