7095a5e4dfa98ec1c4fe3053a01086189d93ab75
Vonney <davonnejv@gmail.com>
2023-08-02 23:39:23 -0500

    adding back node_modules, accidentally deleted it

--- react-router/src/components/Home.jsx
+++ react-router/src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
    <>
       <div className="home">
         <h1 className="homePage">Home</h1>
+        <img src="/images/logo.png" alt="Logo" className="logo"/>
         <img src="../public/images/blackRanger.png" className="powerRangerImage"/>
      </div>
    </>
