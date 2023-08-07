0e3cf44ce1ce920bea3bdb763cdbface67b939e0
Vonney <davonnejv@gmail.com>
2023-08-07 07:22:29 -0500

    something went wrong with my continous deployment

--- react-router/src/components/NavBar.jsx
+++ react-router/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
       <nav>
         <ul className="myLinks">
           <div className="myTitle">Mighty Morphin Power Rangers</div>
-            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/home">Home</Link></li> 
             <li><Link to="/blue">Blue</Link></li>
             <li><Link to="/red">Red</Link></li>
             <li><Link to="/pink">Pink</Link></li>
