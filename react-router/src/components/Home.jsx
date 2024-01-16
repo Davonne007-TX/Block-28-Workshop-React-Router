e2749a802e334a84aa46444e3c8e5bcca8327c42
Vonney <davonnejv@gmail.com>
2024-01-15 18:22:45 -0600

    final commit

--- react-router/src/components/Home.jsx
+++ react-router/src/components/Home.jsx
@@ -4,7 +4,6 @@ function Home() {
   return (
     <>
       <div className="home">
-        <h1 className="homePage">Home</h1>
         <img src="/images/logo.png" alt="Logo" className="logo" />
         <img
           src="/images/blackRanger.png"
